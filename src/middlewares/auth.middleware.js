8ecc8e0678831500ab15c2533acdd74be3400228
Roshan Karki <roshankarki4956@gmail.com>
2026-09-12 23:52:05 +0545

    making the account controller and making the auth middleware for the token validation

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
+
+
+
+async function authMiddleware(req,res,next) {
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+
+    if (!token) {
+        res.status(401).json({
+            message: "Unauthorized access, token is missing"
+        })
+    }
+
+    try {
+        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findById(decoded.userId)
+
+        req.user = user
+
+        return next()
+
+    } catch (err) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid"
+        })
+    }
+}
+
+module.exports = {authMiddleware}
