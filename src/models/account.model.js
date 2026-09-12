6d8c153f89f4d3781f2eac06026e0a2184039071
Roshan Karki <roshankarki4956@gmail.com>
2026-09-13 00:24:19 +0545

    making the account routes and models and controllers for the user of the logged in session

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -8,10 +8,13 @@ const accountSchema = new mongoose.Schema({
         index: true
     },
     status: {
+        type:String,
         enum:{
             values: ["ACTIVE","FROZEN","CLOSED"],
-            message: "Status can be either ACTIVE, FRIZEN or CLOSED"
-        }
+            message: "Status can be either ACTIVE, FROZEN or CLOSED",
+        },
+        default: "ACTIVE"
+
     },
     currency:{
         type:String,
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const { authMiddleware } = require('../middlewares/auth.middleware');
+const { createAccountController } = require('../controllers/account.controller');
 
 const router = express.Router();
 
+
+/**
+ * - POST /api/accounts
+ * - Creates a new account
+ * - It is a Protected Route
+ */
+
+router.post('/',authMiddleware, createAccountController)
 
 module.exports = router
 
