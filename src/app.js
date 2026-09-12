371de3262310c2dfc138142226b3c6ad00dfaab3
Roshan Karki <roshankarki4956@gmail.com>
2026-09-12 23:22:31 +0545

    making the account model schema and its router

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const authRouter = require('./routes/auth.routes');
 const cookieParser = require('cookie-parser');
 
-
 const app = express();
-app.use(express.json())
 
+
+app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api/auth',authRouter) // /api/auth
+/**
+ * - Routes
+ */
+const authRouter = require('./routes/auth.routes');
+const accountRouter = require('./routes/account.routes')
 
+/**
+ * - Use Routes
+ */
+app.use('/api/auth',authRouter) // /api/auth
+app.use('/api/accounts',accountRouter) // /api/accounts
 
 module.exports = app;
