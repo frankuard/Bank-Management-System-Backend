aab95179e7a444b6b68ec1dd635677a407307c8c
Roshan Karki <roshankarki4956@gmail.com>
2026-09-13 23:46:04 +0545

    creating a transaction session to complete creation of transaction along with debit and credit ledger entry

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -2,7 +2,7 @@ const accountModel = require("../models/account.model");
 const ledgerModel = require("../models/ledger.model");
 const transactionModel = require("../models/transaction.model");
 const { sendRegistrationEmail } = require("../services/email.service");
-
+const mongoose = require('mongoose');
 
 /**
  * - Create a new transaction
@@ -108,4 +108,39 @@ async function createTransaction(req,res) {
             message: `Insufficient balance. Current balance is ${balance}. Requested amount is ${amount}`
         })
     }
+
+    /**
+     * 5. Create transaction (PENDING)
+     */
+
+    const session = await mongoose.startSession()
+    session.startTransaction()
+
+    let newTransaction = await transactionModel.create({
+        fromAccount,
+        toAccount,
+        amount,
+        idempotencyKey,
+        status: "PENDING"
+    },{session} )
+
+    const debitLedgerEntry = await ledgerModel.create({
+        account: fromAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type:"DEBIT",
+    },{session})
+
+    const creditLedgerEntry = await ledgerModel.create({
+        account: toAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type:"CREDIT",
+        
+    },{session})
+
+    transaction.status = "COMPLETED"
+
+    await transaction.save({session})
+
 }
